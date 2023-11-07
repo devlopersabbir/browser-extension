428b527cb72ceb3f1b60c5917776d5ad8e8abf74
devlopersabbir <devlopersabbir@gmail.com>
2023-11-07 19:46:40 +0600

    🌼 Tip created with match ❄️

--- (04) omnibox navigate/sw-tips.js
+++ (04) omnibox navigate/sw-tips.js
@@ -8,3 +8,29 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
       .catch((er) => console.log("Fail to set", er));
   }
 });
+
+// Fetch tip & save in storage
+const updateTip = async () => {
+  const response = await fetch("https://extension-tips.glitch.me/tips.json");
+  const tips = await response.json();
+  const randomIndex = Math.floor(Math.random() * tips.length);
+  return chrome.storage.local.set({ tip: tips[randomIndex] });
+};
+
+const ALARM_NAME = "tip";
+
+async function createAlarm() {
+  const alarm = await chrome.alarms.get(ALARM_NAME);
+  if (!alarm) {
+    chrome.alarm.create({
+      delayInMinutes: 1,
+      periodInMinutes: 1440,
+    });
+    updateTip();
+  }
+}
+
+createAlarm();
+
+// Update tip once a day
+chrome.alarms.onAlarm.addListener(updateTip);
